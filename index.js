f1bed7f199e135461968188fd4ad49f1eec3e457
Comus Leong <comus.leong@gmail.com>
2019-11-20 07:05:03 +0800

    update

--- index.js
+++ index.js
@@ -52,21 +52,33 @@ sensor.on('reading', r => {
   logger.info(JSON.stringify(r));
 });
 
-var SerialPort = require('serialport');
-var port = new SerialPort.SerialPort('/dev/ttyACM0', { // change path
-  baudrate: 9600,
-  parser: SerialPort.parsers.readline('\r\n')
+const SerialPort = require("serialport");
+const SerialPortParser = require("@serialport/parser-readline");
+const GPS = require("gps");
+
+const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 });
+const gps = new GPS();
+const parser = port.pipe(new SerialPortParser());
+
+function getAddressInformation(latitude, longitude) {
+  logger.info(JSON.stringify({ latitude, longitude }));
+}
+gps.on("data", async data => {
+  if(data.type == "GGA") {
+      if(data.quality != null) {
+          let address = await getAddressInformation(data.lat, data.lon);
+          console.log(address.Label + " [" + data.lat + ", " + data.lon + "]");
+          logger.info(address.Label + " [" + data.lat + ", " + data.lon + "]");
+      } else {
+          console.log("no gps fix available");
+          logger.info("no gps fix available");
+      }
+  }
 });
-
-var GPS = require('gps');
-var gps = new GPS;
-
-gps.on('data', function(data) {
-  console.log(data, gps.state);
-  logger.info(JSON.stringify(data));
-  logger.info(JSON.stringify(gps.state));
-});
-
-port.on('data', function(data) {
-  gps.updatePartial(data);
+parser.on("data", data => {
+  try {
+      gps.update(data);
+  } catch (e) {
+      throw e;
+  }
 });
